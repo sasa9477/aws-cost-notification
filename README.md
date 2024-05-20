fe8a9665498f63096c5f11177b255ac43c83b407
sasa9477 <sasa9477@outlook.com>
2024-05-21 00:48:02 +0900

    cdk 構成図の追加と README の追加

--- README.md
+++ README.md
@@ -1,14 +1,95 @@
-# Welcome to your CDK TypeScript project
+# aws-cost-notification
 
-This is a blank project for CDK development with TypeScript.
+AWS の使用コストを LINE に通知する aws cdk のレポジトリです。
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## 機能
 
-## Useful commands
+- 通知スケジュールを設定し、使用しているコストを LINE に通知します。
+- 予算を設定し、コストが閾値を超えた場合に LINE に通知します。
+- コスト異常検出を設定し、異常を検出した場合に LINE に通知します。
+- Exchange Rates API を使用して為替レートを取得し、日本円の予想額を表示します。
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+## 事前設定
+
+### 環境変数の設定
+
+`.env` ファイルを作成してアクセストークンを設定します。
+
+```bash
+# LINE Notify のアクセストークン
+LINE_NOTIFY_TOKEN=
+# Exchange Rates API のアクセストークン
+EXCHANGE_RATE_API_KEY=
+```
+
+#### LINE Notify の設定
+
+LINE Notify に登録してアクセストークンを発行し、環境変数に設定してください。（必須）  
+https://notify-bot.line.me/
+
+#### Exchange Rates API の設定
+
+exchangerates に登録してアクセストークンを発行し、環境変数に設定してください。  
+トークンが無い場合は USD と JPY の為替変換は行いません。  
+https://exchangeratesapi.io/
+
+### graphviz のインストール
+
+cdk 構成図を作成するため graphviz をインストールしてください。  
+構成図の作成は、エラーにならないため必須ではありません。
+
+```
+brew install graphviz
+```
+
+Homebrew を使用してインストールできますが、バイナリファイルをダウンロードすることもできます。  
+https://graphviz.org/download/
+
+## 構成の変更
+
+`src/config/config.ts` を編集して構成を変更できます。
+
+### 構成値
+
+```
+/**
+  * コスト通知スケジュールの設定
+  */
+constNotificationScheduleConfig: {
+  /**
+    * 通知スケジュール
+    */
+  scheduleExpression: string;
+};
+/**
+  * 予算通知の設定
+  */
+budgetAlartConfig: {
+  /**
+    * 予算額 (USD)
+    */
+  budgetAmount: number;
+  /**
+    * 実績金額のアラート閾値 (%)
+    */
+  actualAmountCostAlertThreshold: number;
+  /**
+    * 予測金額のアラート閾値 (%)
+    */
+  forecastedAmountCostAlertThreshold: number;
+};
+/**
+  * コスト異常通知の設定
+  */
+costAnomalyNotificationConfig: {
+  // 有効にするかどうか
+  enebled: boolean;
+  // 予想支出の異常通知アラートの閾値 (USD)
+  forecastedAmountCostAlertThreshold: number;
+};
+```
+
+## AWS 構成図
+
+![cdk-diagram-light](cdkgraph/diagram.compact.light.png#gh-light-mode-only)
+![cdk-diagram-dark](cdkgraph/diagram.compact.dark.png#gh-dark-mode-only)
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { CdkGraph, FilterPreset } from "@aws/pdk/cdk-graph";
+import { CdkGraphDiagramPlugin } from "@aws/pdk/cdk-graph-plugin-diagram";
 import * as cdk from "aws-cdk-lib";
 import { AwsSolutionsChecks } from "cdk-nag";
 import * as dotenv from "dotenv";
@@ -8,33 +10,69 @@ import { AwsCostNotificationStack } from "./stacks/AwsCostNotificationStack";
 
 dotenv.config();
 
-const app = new cdk.App();
+(async () => {
+  const app = new cdk.App();
 
-const awsCostNotificationStack = new AwsCostNotificationStack(app, "AwsCostNotificationStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: "ap-northeast-1",
-  },
-  config,
-  crossRegionReferences: true,
-});
-
-// MonitorDimension が Serive のモニターは、AWS アカウント に 1つしか作成できないので、コンフィグで有効になっている場合のみ作成する
-// また、異常検知の通知は us-east-1 でのみサポートされているため、スタックを分ける
-if (config.costAnomalyNotificationConfig.enebled) {
-  const awsCostAnomalyNotificationStack = new AwsCostAnomalyNotificationStack(app, "AwsCostAnomalyNotificationStack", {
+  const awsCostNotificationStack = new AwsCostNotificationStack(app, "AwsCostNotificationStack", {
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: "us-east-1",
+      region: "ap-northeast-1",
     },
     config,
     crossRegionReferences: true,
-    notificationTopic: awsCostNotificationStack.notificationTopic,
   });
 
-  // 依存関係を追加
-  awsCostAnomalyNotificationStack.addDependency(awsCostNotificationStack);
-}
+  // MonitorDimension が Serive のモニターは、AWS アカウント に 1つしか作成できないので、コンフィグで有効になっている場合のみ作成する
+  // また、異常検知の通知は us-east-1 でのみサポートされているため、スタックを分ける
+  if (config.costAnomalyNotificationConfig.enebled) {
+    const awsCostAnomalyNotificationStack = new AwsCostAnomalyNotificationStack(
+      app,
+      "AwsCostAnomalyNotificationStack",
+      {
+        env: {
+          account: process.env.CDK_DEFAULT_ACCOUNT,
+          region: "us-east-1",
+        },
+        config,
+        crossRegionReferences: true,
+        notificationTopic: awsCostNotificationStack.notificationTopic,
+      },
+    );
 
-// AWS のセキュリティマトリックスのセキュリティを確認する
-cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+    // 依存関係を追加
+    awsCostAnomalyNotificationStack.addDependency(awsCostNotificationStack);
+  }
+
+  // AWS のセキュリティマトリックスのセキュリティを確認する
+  cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+
+  // CDK のグラフを出力する
+  const graph = new CdkGraph(app, {
+    plugins: [
+      new CdkGraphDiagramPlugin({
+        diagrams: [
+          {
+            name: "compact.light",
+            title: "AWS Cost Notification",
+            theme: "light",
+            filterPlan: {
+              preset: FilterPreset.COMPACT,
+            },
+          },
+          {
+            name: "compact.dark",
+            title: "AWS Cost Notification",
+            theme: "dark",
+            filterPlan: {
+              preset: FilterPreset.COMPACT,
+            },
+          },
+        ],
+      }),
+    ],
+  });
+
+  app.synth();
+
+  await graph.report();
+})();
